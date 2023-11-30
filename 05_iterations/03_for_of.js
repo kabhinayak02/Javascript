db8a077e7a11c31c11da1bc62a579a97869bec2c
Abhinay Kumar <kabhinayak02@gmail.com>
2023-11-30 23:32:22 +0530

    second commit

---
+++ 05_iterations/03_for_of.js
@@ -0,0 +1,51 @@
+// for-of loop
+
+// Array of string, Array of object
+// ["", "", ""]
+// [{}, {}, {}]
+
+// ----------------------------------------------------------------------------------
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// ----------------------------------------------------------------------------------
+
+// Maps
+// Map object holds unique key-value pairs
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+// -> IN :- India
+// USA :- United States of America
+// Fr :- France
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+}
+// -> myObject is not iterable
+
+// Maps and iterable but objects are not iterable
---
+++ 05_iterations/06_filter.js
@@ -0,0 +1,57 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+const values = coding.forEach( (item) => {
+    // console.log(item);
+    // return item
+} )
+
+// console.log(values);
+// -> undifined
+
+// ----------------------------------------------------------------------------------
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+// console.log(newNums);
+// -> [ 5, 6, 7, 8, 9, 10 ]
+
+// we can use return with filter but not with forEach
+
+// ----------------------------------------------------------------------------------
+
+// but there is other ways to do same work with forEach also lets see below examples.
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNums = []
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+} )
+
+console.log(newNums);
+// -> [ 5, 6, 7, 8, 9, 10 ]
+
+// ----------------------------------------------------------------------------------
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+//let userBooks = books.filter( (bk) => { 
+//   return bk.publish >= 1995 && bk.genre === "History"
+// })
+  console.log(userBooks);
